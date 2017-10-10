3280c0853b73f4a3b890670b4f934e911d99341a
Stefan Motko <stefanmotko@gmail.com>
2017-10-10 10:16:37 +0200

    Added .gitignore, diagram json structure template

---
+++ diagram-json.js
@@ -0,0 +1,47 @@
+/*diagram*/
+{
+	metadata: {},
+	layers: [
+		/*layer*/
+		{
+			lifelines: [
+				/*lifeline*/
+				{
+					name: "string",
+					type: "string",
+					occurenceSpecifications: [
+						/*occurence specification*/
+						{
+
+						}
+					]
+				}
+			],
+			combinedFragments: [
+			/*combined fragment*/
+			{
+				interactionOperands: [
+					/*interactionOperand*/
+					{
+						interactionConstraint: "string",
+						combinedFragments: [
+							/*combined fragment*/
+						],
+						messages: []
+					}
+				]
+				interactionOperator: "string"
+			}
+			],
+			messages: [
+			/*message*/
+			{
+				name: "string",
+				kind: ???,
+				sort: ???
+			}
+			]
+		}
+	],
+	// messages3d: []
+}
