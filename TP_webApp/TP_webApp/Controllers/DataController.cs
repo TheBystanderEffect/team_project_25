d95416ac8c742c780b610f784a3976c33e4a3d68
Martin Dieska <martin.dieska@gmail.com>
2017-10-23 16:01:48 +0200

    pod uz

--- TP_webApp/TP_webApp/Controllers/DataController.cs
+++ TP_webApp/TP_webApp/Controllers/DataController.cs
@@ -6,14 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Bson.IO;
+using TP_webApp.Model;
+
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
-    public class ValuesController : Controller
+    [Route("api/[controller]/[action]")]
+    public class DataController : Controller
     {
-        // GET api/values
+     /*   // GET api/values
         [HttpGet]
+        [ActionName("diagram")]
         public List<String> Get()
         {
             var client = new MongoClient();
@@ -27,10 +30,35 @@ namespace API.Controllers
                 wanted.Add(person.ToJson());
             }
             return wanted;
+        }*/
+
+        
+
+        [HttpPost]
+        [ActionName("message")]
+        public void SaveMessage([FromBody]String message)
+        {
+            var newMessage = Newtonsoft.Json.JsonConvert.DeserializeObject<Message>(message);
+            Console.WriteLine(newMessage.name);
         }
 
-        // GET api/values/5
+        [HttpGet] 
+        [ActionName("message")]
+        public String SendMessage()
+        {
+            
+            Message message = new Message("sprava", kinds.complete, sorts.createMessage, 
+                new Lifeline(new Position(12,13.5,22.1,0,0)), new Lifeline(new Position(12, 18.55, 22.1,0,0)));
+
+            var jsonik= Newtonsoft.Json.JsonConvert.SerializeObject(message);
+
+            return jsonik;
+        }
+        /*****************************ZISKANIE A ULOZENIE DIAGRAMU****************************************/
+
+        // GET api/data/diagram/5
         [HttpGet("{id}")]
+        [ActionName("diagram")]
         public async Task<String> Get(int id)
         {
             var client = new MongoClient();
@@ -43,20 +71,18 @@ namespace API.Controllers
             return diagram;
         }
 
-        // POST api/values
+
+        // POST api/data/diagram
         [HttpPost]
-        public string Post(string name, string height)
+        [ActionName("diagram")]
+        public void Post([FromBody]String test_json)
         {
-
             var client = new MongoClient();
-            var db = client.GetDatabase("team");
-            var coll = db.GetCollection<BsonDocument>("testing");
-            string test_js = "{\"name\" : \"" + name + "\", \"height\" : \""+ height+"\" }";
+            var db = client.GetDatabase("tp");
+            var coll = db.GetCollection<BsonDocument>("diagrams");
 
-            var doc = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(test_js);
+            var doc = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(test_json);
             coll.InsertOneAsync(doc);
-
-            return test_js + '\n' + "Successfully added to database";
         }
 
         // PUT api/values/5
