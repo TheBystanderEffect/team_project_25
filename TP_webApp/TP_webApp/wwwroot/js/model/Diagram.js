2a3db837d7fd965ef42d676cbbe921034ac9217f
Ronald Demeter <darksider458@gmail.com>
2017-10-23 09:30:16 +0200

    Ts model

---
+++ TP_webApp/TP_webApp/wwwroot/js/model/Diagram.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Diagram = /** @class */ (function () {
+    function Diagram() {
+    }
+    Object.defineProperty(Diagram.prototype, "layers", {
+        get: function () {
+            return this._layers;
+        },
+        set: function (layers) {
+            this._layers = layers;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Diagram;
+}());
+exports.Diagram = Diagram;
+//# sourceMappingURL=Diagram.js.map
