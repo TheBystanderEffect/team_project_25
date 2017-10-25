6828f5118343912ee8883f1ea6ae20733162e3fd
Stefan Motko <stefanmotko@gmail.com>
2017-10-25 10:51:51 +0200

    [TOOLS]Changed shebang lines, modified start script to attempt to start mongod, changed typescript compilation target to ES6

--- TP_webApp/TP_webApp/start.sh
+++ TP_webApp/TP_webApp/start.sh
@@ -1,4 +1,5 @@
-#!bash
+#!/usr/bin/env bash
 
+mongod || echo "Mongod was unable to start automatically - please start mongod manually" &
 ../../node_modules/.bin/tsc --watch &
 dotnet run --project TP_webApp.csproj
