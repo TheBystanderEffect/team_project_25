9d11d95406516e147cbbb60d3fc670d0da5593c3
Martin Dieska <martin.dieska@gmail.com>
2017-12-06 21:42:57 +0100

    lifeline controller parcial modifications (put, post) finished

--- TP_webApp/TP_webApp/Controllers/LifelineController.cs
+++ TP_webApp/TP_webApp/Controllers/LifelineController.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json.Linq;
+using TP_webApp.Model;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
 
 namespace TP_webApp.Controllers
 {
@@ -28,16 +32,56 @@ namespace TP_webApp.Controllers
         
         // POST: api/Lifeline
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task<string> Post([FromBody]string args)
         {
+            JObject obj = JObject.Parse(args);
+
+            List<int> ids = Newtonsoft.Json.JsonConvert.DeserializeObject<List<int>>(obj["pole"].ToString());
+            Lifeline newLifeline = obj["objekt"].ToObject<Lifeline>();
+
+            int diag = ids[0];
+            var client = new MongoClient();
+            var db = client.GetDatabase("tp");
+            var coll = db.GetCollection<BsonDocument>("diagrams");
+            var filter = Builders<BsonDocument>.Filter.Eq("id", diag);
+            var data = await coll.Find(filter).SingleAsync();
+            var diagram = data.ToJson(new JsonWriterSettings { OutputMode = JsonOutputMode.Strict });
+
+            Diagram d = Newtonsoft.Json.JsonConvert.DeserializeObject<Diagram>(diagram);
+            d.layers[ids[1]].lifelines.Insert(ids[2], newLifeline);
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(d);
+            var doc = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(json);
+            await coll.ReplaceOneAsync(filter, doc);
+            return json;
         }
         
         // PUT: api/Lifeline/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]Lifeline)
+        public async Task<string> Put([FromBody]string args)
         {
+            JObject obj = JObject.Parse(args);
+
+            List<int> ids = Newtonsoft.Json.JsonConvert.DeserializeObject<List<int>>(obj["pole"].ToString());
+            Lifeline newLifeline = obj["objekt"].ToObject<Lifeline>();
+
+            int diagId = ids[0];
+            var client = new MongoClient();
+            var db = client.GetDatabase("tp");
+            var coll = db.GetCollection<BsonDocument>("diagrams");
+            var filter = Builders<BsonDocument>.Filter.Eq("id", diagId);
+            var data = await coll.Find(filter).SingleAsync();
+            var diagram = data.ToJson(new JsonWriterSettings { OutputMode = JsonOutputMode.Strict });
+
+            Diagram d = Newtonsoft.Json.JsonConvert.DeserializeObject<Diagram>(diagram);
+            d.layers[ids[1]].lifelines[ids[2]] = newLifeline;
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(d);
+            var doc = MongoDB.Bson.Serialization.BsonSerializer.Deserialize<BsonDocument>(json);
+            await coll.ReplaceOneAsync(filter, doc);
+            return json;
         }
-        
+
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
