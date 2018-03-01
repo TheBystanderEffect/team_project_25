65676500e1d78eaa1e16374c9ce289b20af4c3f0
Stefan Motko <stefanmotko@gmail.com>
2018-03-01 15:31:40 +0100

    [FIX]Clean commited JavaScript

--- TP_webApp/TP_webApp/wwwroot/js/app/model/InteractionOperand.js
+++
@@ -1,8 +0,0 @@
-export class InteractionOperand {
-    constructor(interactionConstraint, combinedFragment, messages) {
-        this.interactionConstraint = interactionConstraint;
-        this.combinedFragment = combinedFragment;
-        this.messages = messages;
-    }
-}
-//# sourceMappingURL=InteractionOperand.js.map
