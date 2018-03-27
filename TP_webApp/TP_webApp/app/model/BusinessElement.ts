db836a37fd0165e200dcb17b5f3f3cfd3147b735
PeterPsota <peter.n.psota@gmail.com>
2018-03-27 11:58:14 +0200

    [REVERT]revert previous commit

--- TP_webApp/TP_webApp/app/model/BusinessElement.ts
+++ TP_webApp/TP_webApp/app/model/BusinessElement.ts
@@ -1,19 +1,10 @@
 import { GraphicElement } from "../view/GraphicElement";
-import { ViewMap } from "../view/TypeMapping";
 
 export abstract class BusinessElement {
 
     protected __graphicElement: GraphicElement;
 
     public get graphicElement(): GraphicElement {
-        if (!this.__graphicElement) {
-            for (let [ modelType, constructorFunction ] of ViewMap) {
-                if (this instanceof modelType) {
-                    this.__graphicElement = constructorFunction(this);
-                    break;
-                }
-            }
-        }
         return this.__graphicElement;
     }
 
